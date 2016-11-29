ccc1d01348c5feecba27b7f00afeea9faa29270a
josephnalluri <joseph.nalluri@gmail.com>
2016-11-28 20:08:59 -0500

    Fixed the functionality of dropdowns to new table/database.

--- drawGraph.js
+++ drawGraph.js
@@ -1,8 +1,7 @@
-function createGraph(jsonData,tabDiv)
+function createGraph(jsonData,tabDiv,counterID)
 {
-  // Checking the parameters
-  //console.log(jsonData);
-  //console.log(tabDiv);
+
+ // console.log(jsonData);
 
   jsonData.sort(function(a,b){if(a.source>b.source){return 1;}
   else if(a.source<b.source){return-1;}
@@ -49,17 +48,10 @@ for(var i=0;i<jsonData.length;i++) {
 
     for(var i=0;i<jsonData.length;i++){arrows[i]=jsonData[i].type;};
 
+    //var w=1000,h=800;
     var w= 900, h=800;
-    
-    // Switch case to associate variable 'w' with the correct calling object.
-    // If the function drawGraph is called from tab 'Individual Disease', v = #graph object
-    // If the funtion drawGraph is called from tab 'Disease Category', v = #disease_category_graph object 
-    switch(tabDiv)
-    {
-      case "#graph": var w = document.getElementById("graph").offsetWidth; break;
-      case "#disease_category_graph": var w = document.getElementById("disease_category_graph").offsetWidth; break;
-    }
- 
+    var w = document.getElementById("graph").offsetWidth;
+
     var force=d3.layout.force()
     .nodes(d3.values(nodes))
     .links(jsonData)
@@ -119,7 +111,8 @@ for(var i=0;i<jsonData.length;i++) {
     function tick() {
         path.attr("d",function(d)
         {
-          var dx=d.target.x-d.source.x,dy=d.target.y-d.source.y, dr=1000/d.linknum;
+          var dx=d.target.x-d.source.x,dy=d.target.y-d.source.y,
+          dr=1000/d.linknum;
           return"M"+d.source.x+","+d.source.y+"A"+dr+","+dr+" 0 0,1 "+d.target.x+","+d.target.y;
         });
 
