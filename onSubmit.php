2302e607e84ef24f439a27050ca6ca902a8c998e
josephnalluri <joseph.nalluri@gmail.com>
2017-02-10 15:20:16 -0500

    Updated home page SEO

--- onSubmit.php
+++ onSubmit.php
@@ -7,24 +7,12 @@ $counterID = $_POST["counterID"];
 
 if($counterID == 1)
 {
-	if(ISSET($_POST["disSelected"]) and ISSET($_POST["minSelected"]) and ISSET($_POST["maxSelected"]))
+	if(ISSET($_POST["disSelected"]))
 	{
 	 $disease = mysqli_real_escape_string($dbConnect, $_POST["disSelected"]);
-	 $min = mysqli_real_escape_string($dbConnect, $_POST["minSelected"]);
-	 $max = mysqli_real_escape_string($dbConnect, $_POST["maxSelected"]);
-	 $query = 
-		"SELECT 
-				m1 AS source,
-				m2 AS target,
-				score AS type 
-		FROM consensus_avg_pancancer 
-		WHERE d1=d2 
-		  AND d1= '".$disease."'
-		  AND score<'".$max."'
-		  AND score>'".$min."'
-		ORDER by type DESC limit 500";
+	 $query = "SELECT mirna1 AS source, mirna2 AS target, score AS type	FROM mirna_rescored WHERE disease='".$disease."' ORDER by type DESC limit 500";
 
-     $queryCSV = "SELECT m1 AS source, m2 AS target, score AS type FROM consensus_avg_pancancer WHERE d1=d2 AND d1='".$disease."' AND score<'".$max."' AND score>'".$min."' ORDER by type into outfile '/var/www/bnet.egr.vcu.edu/public_html/miRsig/CSV/network.csv' fields terminated by ','";
+     $queryCSV = "SELECT mirna1 AS source, mirna2 AS target, score AS type FROM mirna_rescored WHERE disease='".$disease."' ORDER by type into outfile '/var/www/bnet.egr.vcu.edu/public_html/mirid/CSV/network.csv' fields terminated by ','";
 	
 	 $queryResult = mysqli_query($dbConnect, $query);
      $queryResultCSV = mysqli_query($dbConnect, $queryCSV);
@@ -42,16 +30,14 @@ if($counterID == 1)
 
 elseif($counterID == 2)
 {
-   if(ISSET($_POST["disSelected"]) and ISSET($_POST["disSelected2"]) and ISSET($_POST["minSelected"]) and ISSET($_POST["maxSelected"]))
+   if(ISSET($_POST["disSelected"]) and ISSET($_POST["disSelected2"]))
 	{
 	 $disease = mysqli_real_escape_string($dbConnect, $_POST["disSelected"]);
 	 $disease2 = mysqli_real_escape_string($dbConnect, $_POST["disSelected2"]);
-	 $min = mysqli_real_escape_string($dbConnect, $_POST["minSelected"]);
-	 $max = mysqli_real_escape_string($dbConnect, $_POST["maxSelected"]);
 	 
-	 $query = "select a.m1 as source, a.m2 as target, a.score as type from (select m1, m2, score from consensus_avg_pancancer where d1='".$disease."' and score<'".$max."' AND score>'".$min."')a inner join (select m1, m2, score from consensus_avg_pancancer where d1='".$disease2."' and score<'".$max."' AND score>'".$min."')b using (m1,m2) order by type desc limit 250";
+	 $query = "select a.mirna1 as source, a.mirna2 as target, a.score as type from (select mirna1, mirna2, score from mirna_rescored where disease='".$disease."')a inner join (select mirna1, mirna2, score from mirna_rescored where disease='".$disease2."')b using (mirna1,mirna2) order by type desc limit 500";
 	 
-	 $queryCSV = "select a.m1 as source, a.m2 as target, a.score as type from (select m1, m2, score from consensus_avg_pancancer where d1='".$disease."' and score<'".$max."' AND score>'".$min."')a inner join (select m1, m2, score from consensus_avg_pancancer where d1='".$disease2."' and score<'".$max."' AND score>'".$min."')b using (m1,m2) order by type desc limit 250 into outfile '/var/www/bnet.egr.vcu.edu/public_html/miRsig/CSV/network.csv' fields terminated by ','";
+	 $queryCSV = "select a.mirna1 as source, a.mirna2 as target, a.score as type from (select mirna1, mirna2, score from mirna_rescored where disease='".$disease."')a inner join (select mirna1, mirna2, score from mirna_rescored where disease='".$disease2."')b using (mirna1,mirna2) order by type desc into outfile '/var/www/bnet.egr.vcu.edu/public_html/mirid/CSV/network.csv' fields terminated by ','";
 	 
 	 $queryResult = mysqli_query($dbConnect, $query);
 	 $queryResultCSV = mysqli_query($dbConnect, $queryCSV);
@@ -70,17 +56,15 @@ elseif($counterID == 2)
 
 elseif($counterID == 3)
 {
-   if(ISSET($_POST["disSelected"]) and ISSET($_POST["disSelected2"]) and ISSET($_POST["disSelected3"]) and ISSET($_POST["minSelected"]) and ISSET($_POST["maxSelected"]))
+   if(ISSET($_POST["disSelected"]) and ISSET($_POST["disSelected2"]) and ISSET($_POST["disSelected3"]) )
 	{
 	 $disease = mysqli_real_escape_string($dbConnect, $_POST["disSelected"]);
 	 $disease2 = mysqli_real_escape_string($dbConnect, $_POST["disSelected2"]);
 	 $disease3 = mysqli_real_escape_string($dbConnect, $_POST["disSelected3"]);
-	 $min = mysqli_real_escape_string($dbConnect, $_POST["minSelected"]);
-	 $max = mysqli_real_escape_string($dbConnect, $_POST["maxSelected"]);
 	
-	 $query = "select a.m1 as source, a.m2 as target, a.score as type from (select m1, m2, score from consensus_avg_pancancer where d1='".$disease."' and score<'".$max."' AND score>'".$min."')a inner join (select m1, m2, score from consensus_avg_pancancer where d1='".$disease2."' and score<'".$max."' AND score>'".$min."')b using (m1,m2) inner join (select m1, m2, score from consensus_avg_pancancer where d1='".$disease3."' and score<'".$max."' AND score>'".$min."')c using (m1,m2) order by type desc limit 250";
+	 $query = "select a.mirna1 as source, a.mirna2 as target, a.score as type from (select mirna1, mirna2, score from mirna_rescored where disease='".$disease."')a inner join (select mirna1, mirna2, score from mirna_rescored where disease='".$disease2."')b using (mirna1,mirna2) inner join (select mirna1, mirna2, score from mirna_rescored where disease='".$disease3."')c using (mirna1,mirna2) order by type desc limit 2500";
 	 
-	 $queryCSV = "select a.m1 as source, a.m2 as target, a.score as type from (select m1, m2, score from consensus_avg_pancancer where d1='".$disease."' and score<'".$max."' AND score>'".$min."')a inner join (select m1, m2, score from consensus_avg_pancancer where d1='".$disease2."' and score<'".$max."' AND score>'".$min."')b using (m1,m2) inner join (select m1, m2, score from consensus_avg_pancancer where d1='".$disease3."' and score<'".$max."' AND score>'".$min."')c using (m1,m2) order by type desc limit 250 into outfile '/var/www/bnet.egr.vcu.edu/public_html/miRsig/CSV/network.csv' fields terminated by ','";
+	 $queryCSV = "select a.mirna1 as source, a.mirna2 as target, a.score as type from (select mirna1, mirna2, score from mirna_rescored where disease='".$disease."')a inner join (select mirna1, mirna2, score from mirna_rescored where disease='".$disease2."')b using (mirna1,mirna2) inner join (select mirna1, mirna2, score from mirna_rescored where disease='".$disease3."')c using (mirna1,mirna2) order by type desc into outfile '/var/www/bnet.egr.vcu.edu/public_html/mirid/CSV/network.csv' fields terminated by ','";
 	 
 	 $queryResult = mysqli_query($dbConnect, $query);
 	 $queryResultCSV = mysqli_query($dbConnect, $queryCSV);
@@ -98,18 +82,16 @@ elseif($counterID == 3)
 }
 elseif($counterID == 4)
 {
-   if(ISSET($_POST["disSelected"]) and ISSET($_POST["disSelected2"]) and ISSET($_POST["disSelected3"]) and ISSET($_POST["disSelected4"]) and ISSET($_POST["minSelected"]) and ISSET($_POST["maxSelected"]))
+   if(ISSET($_POST["disSelected"]) and ISSET($_POST["disSelected2"]) and ISSET($_POST["disSelected3"]) and ISSET($_POST["disSelected4"]))
 	{
 	 $disease = mysqli_real_escape_string($dbConnect, $_POST["disSelected"]);
 	 $disease2 = mysqli_real_escape_string($dbConnect, $_POST["disSelected2"]);
 	 $disease3 = mysqli_real_escape_string($dbConnect, $_POST["disSelected3"]);
 	 $disease4 = mysqli_real_escape_string($dbConnect, $_POST["disSelected4"]);
-	 $min = mysqli_real_escape_string($dbConnect, $_POST["minSelected"]);
-	 $max = mysqli_real_escape_string($dbConnect, $_POST["maxSelected"]);
 			
-	$query = "select a.m1 as source, a.m2 as target, a.score as type from (select m1, m2, score from consensus_avg_pancancer where d1='".$disease."' and score<'".$max."' AND score>'".$min."')a inner join (select m1, m2, score from consensus_avg_pancancer where d1='".$disease2."' and score<'".$max."' AND score>'".$min."')b using (m1,m2) inner join (select m1, m2, score from consensus_avg_pancancer where d1='".$disease3."' and score<'".$max."' AND score>'".$min."')c using (m1,m2) inner join (select m1, m2, score from consensus_avg_pancancer where d1='".$disease4."' and score<'".$max."' AND score>'".$min."')d using (m1,m2) order by type desc limit 250";
+	$query = "select a.mirna1 as source, a.mirna2 as target, a.score as type from (select mirna1, mirna2, score from mirna_rescored where disease='".$disease."')a inner join (select mirna1, mirna2, score from mirna_rescored where disease='".$disease2."')b using (mirna1,mirna2) inner join (select mirna1, mirna2, score from mirna_rescored where disease='".$disease3."')c using (mirna1,mirna2) inner join (select mirna1, mirna2, score from mirna_rescored where disease='".$disease4."')d using (mirna1,mirna2) order by type desc";
 
-    $queryCSV = "select a.m1 as source, a.m2 as target, a.score as type from (select m1, m2, score from consensus_avg_pancancer where d1='".$disease."' and score<'".$max."' AND score>'".$min."')a inner join (select m1, m2, score from consensus_avg_pancancer where d1='".$disease2."' and score<'".$max."' AND score>'".$min."')b using (m1,m2) inner join (select m1, m2, score from consensus_avg_pancancer where d1='".$disease3."' and score<'".$max."' AND score>'".$min."')c using (m1,m2) inner join (select m1, m2, score from consensus_avg_pancancer where d1='".$disease4."' and score<'".$max."' AND score>'".$min."')d using (m1,m2) order by type desc limit 250 into outfile '/var/www/bnet.egr.vcu.edu/public_html/miRsig/CSV/network.csv' fields terminated by ','";	
+    $queryCSV = "select a.mirna1 as source, a.mirna2 as target, a.score as type from (select mirna1, mirna2, score from mirna_rescored where disease='".$disease."')a inner join (select mirna1, mirna2, score from mirna_rescored where disease='".$disease2."')b using (mirna1,mirna2) inner join (select mirna1, mirna2, score from mirna_rescored where disease='".$disease3."')c using (mirna1,mirna2) inner join (select mirna1, mirna2, score from mirna_rescored where disease='".$disease4."')d using (mirna1,mirna2) order by type desc into outfile '/var/www/bnet.egr.vcu.edu/public_html/mirid/CSV/network.csv' fields terminated by ','";	
 
 	$queryResult = mysqli_query($dbConnect, $query);
     $queryResultCSV = mysqli_query($dbConnect, $queryCSV);
@@ -128,19 +110,17 @@ elseif($counterID == 4)
 
 elseif($counterID == 5)
 {
-   if(ISSET($_POST["disSelected"]) and ISSET($_POST["disSelected2"]) and ISSET($_POST["disSelected3"]) and ISSET($_POST["disSelected4"]) and ISSET($_POST["disSelected5"]) and ISSET($_POST["minSelected"]) and ISSET($_POST["maxSelected"]))
+   if(ISSET($_POST["disSelected"]) and ISSET($_POST["disSelected2"]) and ISSET($_POST["disSelected3"]) and ISSET($_POST["disSelected4"]) and ISSET($_POST["disSelected5"]))
 	{
 	 $disease = mysqli_real_escape_string($dbConnect, $_POST["disSelected"]);
 	 $disease2 = mysqli_real_escape_string($dbConnect, $_POST["disSelected2"]);
 	 $disease3 = mysqli_real_escape_string($dbConnect, $_POST["disSelected3"]);
 	 $disease4 = mysqli_real_escape_string($dbConnect, $_POST["disSelected4"]);
 	 $disease5 = mysqli_real_escape_string($dbConnect, $_POST["disSelected5"]);
-	 $min = mysqli_real_escape_string($dbConnect, $_POST["minSelected"]);
-	 $max = mysqli_real_escape_string($dbConnect, $_POST["maxSelected"]);
 	
-	 $query = "select a.m1 as source, a.m2 as target, a.score as type from (select m1, m2, score from consensus_avg_pancancer where d1='".$disease."' and score<'".$max."' AND score>'".$min."')a inner join (select m1, m2, score from consensus_avg_pancancer where d1='".$disease2."' and score<'".$max."' AND score>'".$min."')b using (m1,m2) inner join (select m1, m2, score from consensus_avg_pancancer where d1='".$disease3."' and score<'".$max."' AND score>'".$min."')c using (m1,m2) inner join (select m1, m2, score from consensus_avg_pancancer where d1='".$disease4."' and score<'".$max."' AND score>'".$min."')d using (m1,m2) inner join (select m1, m2, score from consensus_avg_pancancer where d1='".$disease5."' and score<'".$max."' AND score>'".$min."')e using (m1,m2) order by type desc limit 250";	
+	 $query = "select a.mirna1 as source, a.mirna2 as target, a.score as type from (select mirna1, mirna2, score from mirna_rescored where disease='".$disease."')a inner join (select mirna1, mirna2, score from mirna_rescored where disease='".$disease2."')b using (mirna1,mirna2) inner join (select mirna1, mirna2, score from mirna_rescored where disease='".$disease3."')c using (mirna1,mirna2) inner join (select mirna1, mirna2, score from mirna_rescored where disease='".$disease4."')d using (mirna1,mirna2) inner join (select mirna1, mirna2, score from mirna_rescored where disease='".$disease5."')e using (mirna1,mirna2) order by type desc";	
 
-	 $queryCSV = "select a.m1 as source, a.m2 as target, a.score as type from (select m1, m2, score from consensus_avg_pancancer where d1='".$disease."' and score<'".$max."' AND score>'".$min."')a inner join (select m1, m2, score from consensus_avg_pancancer where d1='".$disease2."' and score<'".$max."' AND score>'".$min."')b using (m1,m2) inner join (select m1, m2, score from consensus_avg_pancancer where d1='".$disease3."' and score<'".$max."' AND score>'".$min."')c using (m1,m2) inner join (select m1, m2, score from consensus_avg_pancancer where d1='".$disease4."' and score<'".$max."' AND score>'".$min."')d using (m1,m2) inner join (select m1, m2, score from consensus_avg_pancancer where d1='".$disease5."' and score<'".$max."' AND score>'".$min."')e using (m1,m2) order by type desc limit 250";
+	 $queryCSV = "select a.mirna1 as source, a.mirna2 as target, a.score as type from (select mirna1, mirna2, score from mirna_rescored where disease='".$disease."')a inner join (select mirna1, mirna2, score from mirna_rescored where disease='".$disease2."')b using (mirna1,mirna2) inner join (select mirna1, mirna2, score from mirna_rescored where disease='".$disease3."')c using (mirna1,mirna2) inner join (select mirna1, mirna2, score from mirna_rescored where disease='".$disease4."')d using (mirna1,mirna2) inner join (select mirna1, mirna2, score from mirna_rescored where disease='".$disease5."')e using (mirna1,mirna2) order by type desc into outfile '/var/www/bnet.egr.vcu.edu/public_html/mirid/CSV/network.csv' fields terminated by ','";
 	 
 	 $queryResult = mysqli_query($dbConnect, $query);
 	 $queryResultCSV = mysqli_query($dbConnect, $queryCSV);
